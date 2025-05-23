bc7f3d5883ac12dc75a98587bea165f3601f5a5a
Dafesigo04 <160060204+Dafesigo04@users.noreply.github.com>
2025-05-23 12:12:06 -0500

    Update settings.py

--- dajj/settings.py
+++ dajj/settings.py
@@ -126,18 +126,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = 'static/'
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'), #une la carpeta static
-)
+STATIC_URL = '/static/'
 
-MEDIA_URL= 'media/'
-MEDIAFILES_DIRS=(
-    os.path.join(BASE_DIR, 'media'),
-)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),
+]
 
-MEDIA_ROOT =os.path.join(BASE_DIR, '')#otro cambio
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # carpeta donde collectstatic copia los archivos
+
 MEDIA_URL = '/media/'
+MEDIA_ROOT =os.path.join(BASE_DIR, 'media')
 
 
 # Default primary key field type
